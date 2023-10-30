c008cfe355156608638278c8f795704227aa8594
Beatriz Ferreira <124069060+Beatriz273@users.noreply.github.com>
2023-10-30 12:38:05 +0000

    a

--- Lista 1/ex3.py
+++ Lista 1/ex3.py
@@ -1,10 +1,13 @@
-n  = 50
+n  = 1000000
 cont = 0
+primo = []
 
-for i in range(1,n):
+for i in range(1,n**0.5):
     cont = 0
     for j in range (2, i):
         if (i % j) == 0:
             cont += 1
     if cont == 0:
-        print(f"{i} é primo")
+        primo.append(i)
+
+print(f"Os números primos são: {primo}")
